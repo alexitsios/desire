ffa1717c47c8292d1476eb3cf4aa94413966548f
vitormaduro <vitormaduro006@gmail.com>
2022-07-19 06:51:56 -0300

    Added persistance to the settings menu

--- desireUnity/Assets/Scripts/SettingsMenu.cs
+++ desireUnity/Assets/Scripts/SettingsMenu.cs
@@ -1,12 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
-    public void UpdateSliderLabel(int sliderId)
+	private void Start()
+	{
+		GameObject.Find("LanguageSelect").GetComponent<TMP_Dropdown>().value = PlayerPrefs.GetInt("Language", 1);
+		GameObject.Find("Slider_0").GetComponent<Slider>().value = PlayerPrefs.GetFloat("MasterVolume", 100f);
+		GameObject.Find("Slider_1").GetComponent<Slider>().value = PlayerPrefs.GetFloat("FxVolume", 100f);
+		GameObject.Find("Slider_2").GetComponent<Slider>().value = PlayerPrefs.GetFloat("BgVolume", 100f);
+		GameObject.Find("BeepToggle").GetComponent<Toggle>().isOn = PlayerPrefs.GetInt("Beep", 1) == 1;
+		GameObject.Find("HintsToggle").GetComponent<Toggle>().isOn = PlayerPrefs.GetInt("Hints", 0) == 1;
+	}
+
+	public void UpdateSliderLabel(int sliderId)
 	{
 		var value = GameObject.Find($"Slider_{sliderId}").GetComponent<Slider>().value;
 		GameObject.Find($"SliderLabel_{sliderId}").GetComponent<TextMeshProUGUI>().text = value.ToString();
@@ -14,16 +22,31 @@ public class SettingsMenu : MonoBehaviour
 
 	public void SaveSettings()
 	{
+		var language = GameObject.Find("LanguageSelect").GetComponent<TMP_Dropdown>().value;
+		var masterVolume = GameObject.Find("Slider_0").GetComponent<Slider>().value;
+		var fxVolume = GameObject.Find("Slider_1").GetComponent<Slider>().value;
+		var bgVolume = GameObject.Find("Slider_2").GetComponent<Slider>().value;
+		var beep = GameObject.Find("BeepToggle").GetComponent<Toggle>().isOn ? 1 : 0;
+		var hints = GameObject.Find("HintsToggle").GetComponent<Toggle>().isOn ? 1 : 0;
+
 		var gc = GetComponentInParent<GameManager>();
 		gc.Settings = new Settings
 		{
-			Language = (Language)GameObject.Find("LanguageSelect").GetComponent<TMP_Dropdown>().value,
-			MasterVolume = GameObject.Find("Slider_0").GetComponent<Slider>().value / 100,
-			FXVolume = GameObject.Find("Slider_1").GetComponent<Slider>().value / 100,
-			BGVolume = GameObject.Find("Slider_2").GetComponent<Slider>().value / 100,
-			BeepSound = GameObject.Find("BeepToggle").GetComponent<Toggle>().isOn,
-			ShowHints = GameObject.Find("HintsToggle").GetComponent<Toggle>().isOn
+			Language = (Language) language,
+			MasterVolume = masterVolume,
+			FXVolume = fxVolume,
+			BGVolume = bgVolume,
+			BeepSound = beep == 1,
+			ShowHints = hints == 1
 		};
+
+		PlayerPrefs.SetInt("Language", language);
+		PlayerPrefs.SetFloat("MasterVolume", masterVolume);
+		PlayerPrefs.SetFloat("FxVolume", fxVolume);
+		PlayerPrefs.SetFloat("BgVolume", bgVolume);
+		PlayerPrefs.SetInt("Beep", beep);
+		PlayerPrefs.SetInt("Hints", hints);
+		PlayerPrefs.Save();
 
 		CloseSettingsScreen();
 	}
